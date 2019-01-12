89ca565a6af3d13607009447f4b3a1ad111f6340
Arthur <2864560903@qq.com>
2019-01-12 10:08:19 +0800

    新增测试类

---
+++ src/main/java/heima/itcast/demo/Test.java
@@ -0,0 +1,12 @@
+package heima.itcast.demo;
+
+/**
+ * @Author: hjj
+ * @Date: 2019/1/12 10:07
+ * @Version 1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("123");
+    }
+}
