5b63ae7f4d4d3ccdf3894812c14b3ab4492393c5
Arthur <2864560903@qq.com>
2019-01-12 11:05:42 +0800

    修改测试类

--- src/main/java/heima/itcast/demo/Test.java
+++ src/main/java/heima/itcast/demo/Test.java
@@ -8,5 +8,9 @@ package heima.itcast.demo;
 public class Test {
     public static void main(String[] args) {
         System.out.println(123);
+        System.out.println("aaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaa");
     }
 }
